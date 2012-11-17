b5aa7e5337fb201af36e4751b8f4c1aea7de80ed
Zizhu Dmiy <Zizhu Dmiy@zizhu-pc>
2012-11-17 23:31:36 +0700

    Masukan resource gambar

--- aplikasibdm/gen/com/example/aplikasibdm/R.java
+++ aplikasibdm/gen/com/example/aplikasibdm/R.java
@@ -11,7 +11,31 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int about_biru=0x7f020000;
+        public static final int about_hijau=0x7f020001;
+        public static final int about_merah=0x7f020002;
+        public static final int background=0x7f020003;
+        public static final int background2=0x7f020004;
+        public static final int button_about=0x7f020005;
+        public static final int button_help=0x7f020006;
+        public static final int button_keluar=0x7f020007;
+        public static final int button_kuis=0x7f020008;
+        public static final int button_materi=0x7f020009;
+        public static final int help_biru=0x7f02000a;
+        public static final int help_hijau=0x7f02000b;
+        public static final int help_merah=0x7f02000c;
+        public static final int ic_action_search=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
+        public static final int keluar_biru=0x7f02000f;
+        public static final int keluar_hijau=0x7f020010;
+        public static final int keluar_merah=0x7f020011;
+        public static final int kuis_biru=0x7f020012;
+        public static final int kuis_hijau=0x7f020013;
+        public static final int kuis_merah=0x7f020014;
+        public static final int materi_biru=0x7f020015;
+        public static final int materi_hijau=0x7f020016;
+        public static final int materi_merah=0x7f020017;
+        public static final int splash_screen=0x7f020018;
     }
     public static final class id {
         public static final int menu_settings=0x7f070000;
