6894363c41c281b8329c28e034bab6e15248a92d
Zizhu Dmiy <Zizhu Dmiy@zizhu-pc>
2012-11-19 07:30:41 +0700

    Splash Screen dan Menu Utama

---
+++ aplikasibdm/src/com/example/aplikasibdm/Soal.java
@@ -0,0 +1,28 @@
+package com.example.aplikasibdm;
+
+public class Soal {
+	private String[] soal;
+	private String jawaban;
+	private String[] pilihan;
+	
+	public String[] getSoal() {
+		return soal;
+	}
+	public void setSoal(String[] soal) {
+		this.soal = soal;
+	}
+	public String getJawaban() {
+		return jawaban;
+	}
+	public void setJawaban(String jawaban) {
+		this.jawaban = jawaban;
+	}
+	public String[] getPilihan() {
+		return pilihan;
+	}
+	public void setPilihan(String[] pilihan) {
+		this.pilihan = pilihan;
+	}
+	
+	
+}
